1e2f121a56e29c5ebcd326c3773121d0eeeaf697
William Guo <yueguo@ebay.com>
2017-02-23 01:18:27 -0800

    gitignore

---
+++ core/src/main/java/org/apache/griffin/core/GriffinWebApplication.java
@@ -0,0 +1,12 @@
+package org.apache.griffin.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriffinWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GriffinWebApplication.class, args);
+    }
+}
