56ac6ccd0f2ddfa3b9574f140c566e2f7e2d8e97
William Guo <yueguo@ebay.com>
2017-02-27 23:03:08 -0800

    init

--- service/src/main/java/org/apache/griffin/core/spec/DataAsset.java
+++ service/src/main/java/org/apache/griffin/core/spec/DataAsset.java
@@ -3,8 +3,8 @@ package org.apache.griffin.core.spec;
 
 import java.io.Serializable;
 
-public class DataAsset<T> implements Serializable {
-    private String name;
-    private String org;
-    private String owner;
+public interface DataAsset extends Serializable {
+    public String getName();
+    public String getOrg();
+    public String getOwner();
 }
--- service/src/main/java/org/apache/griffin/core/spec/HiveDataAsset.java
+++ service/src/main/java/org/apache/griffin/core/spec/HiveDataAsset.java
@@ -1,6 +1,24 @@
 package org.apache.griffin.core.spec;
 
 
+/**
+ * DataAsset based on hive metadata.
+ */
 
-public class HiveDataAsset extends  DataAsset {
+public class HiveDataAsset implements DataAsset {
+    public String getName() {
+        return name;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    private String owner;
+    private String org;
+    private String name;
 }
