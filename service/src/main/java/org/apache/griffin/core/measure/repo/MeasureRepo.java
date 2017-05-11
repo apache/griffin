b5fce52475bf275d6ff3fd97a1e0eee7b4a4d3fc
Chen <xiangrchen@lm-shc-16501061.corp.ebay.com>
2017-05-11 12:38:17 +0800

    ut and org controller
    Author: Chen <xiangrchen@lm-shc-16501061.corp.ebay.com>
    
    Closes #23 from justACT/ut.

--- service/src/main/java/org/apache/griffin/core/measure/repo/MeasureRepo.java
+++ service/src/main/java/org/apache/griffin/core/measure/repo/MeasureRepo.java
@@ -2,8 +2,22 @@ package org.apache.griffin.core.measure.repo;
 
 
 import org.apache.griffin.core.measure.Measure;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface MeasureRepo extends CrudRepository<Measure, Long> {
     Measure findByName(String name);
+
+    @Query("select DISTINCT m.organization from Measure m")
+    List<String> findOrganizations();
+
+    @Query("select m.name from Measure m " +
+            "where m.organization= ?1")
+    List<String> findNameByOrganization(String organization);
+
+    @Query("select m.organization from Measure m "+
+            "where m.name= ?1")
+    String findOrgByName(String measureName);
 }
--- service/src/main/java/org/apache/griffin/core/service/GriffinController.java
+++ service/src/main/java/org/apache/griffin/core/service/GriffinController.java
@@ -1,8 +1,15 @@
 package org.apache.griffin.core.service;
 
 
+import org.apache.griffin.core.measure.repo.MeasureRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -13,5 +20,18 @@ public class GriffinController {
         return "0.1.0";
     }
 
+    private static final Logger log = LoggerFactory.getLogger(GriffinController.class);
+    @Autowired
+    MeasureRepo measureRepo;
+
+    @RequestMapping("/org")
+    public List<String> getOrg(){
+        return measureRepo.findOrganizations();
+    }
+
+    @RequestMapping("/org/{org}")
+    public List<String> getMetricNameListByOrg(@PathVariable("org") String org){
+        return measureRepo.findNameByOrganization(org);
+    }
 }
 
