dc65353ea0f2af5d4c1edb486fdbdf5229f5544d
Chen <xiangrchen@lm-shc-16501061.corp.ebay.com>
2017-05-15 13:37:36 +0800

    add orgWithMetrivs API

--- service/src/main/java/org/apache/griffin/core/service/GriffinController.java
+++ service/src/main/java/org/apache/griffin/core/service/GriffinController.java
@@ -2,6 +2,7 @@ package org.apache.griffin.core.service;
 
 
 import org.apache.griffin.core.measure.repo.MeasureRepo;
+import org.apache.griffin.core.util.OrgWithMetrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -25,13 +27,26 @@ public class GriffinController {
     MeasureRepo measureRepo;
 
     @RequestMapping("/org")
-    public List<String> getOrg(){
+    public List<String> getOrgs(){
         return measureRepo.findOrganizations();
     }
 
     @RequestMapping("/org/{org}")
     public List<String> getMetricNameListByOrg(@PathVariable("org") String org){
         return measureRepo.findNameByOrganization(org);
+    }
+    @RequestMapping("/orgWithMetrics")
+    public List<OrgWithMetrics> getOrgsWithMetrics(){
+        List<OrgWithMetrics> orgWithMetricsList=new ArrayList<OrgWithMetrics>();
+        List<String> orgList=measureRepo.findOrganizations();
+        for (String org:orgList){
+            OrgWithMetrics orgWithMetrics=new OrgWithMetrics();
+            orgWithMetrics.setOrg(org);
+            orgWithMetrics.setMeasureName(measureRepo.findNameByOrganization(org));
+            orgWithMetricsList.add(orgWithMetrics);
+        }
+        return orgWithMetricsList;
+
     }
 }
 
