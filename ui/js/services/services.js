78d0a36aa95de1a2ffce57b6012000c27e3b37e3
Liu <lliu13@lm-shc-16501428.corp.ebay.com>
2017-05-17 11:26:09 +0800

    version 1

--- ui/js/services/services.js
+++ ui/js/services/services.js
@@ -25,7 +25,7 @@ define(['./module'], function (services) {
       var BACKEND_SERVER = '';
       var API_ROOT_PATH = '/api/v1';
 //      var ES_SERVER = 'http://10.149.247.156:39200';
-      var ES_SERVER = 'http://10.149.247.156:39200';
+      var ES_SERVER = 'http://localhost:9200';
 
       var config = {
           // URI paths, always have a trailing /
