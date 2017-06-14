742d033463bc4663868feefd9add3f1b518ebd92
Lionel Liu <bhlx3lyx7@163.com>
2017-06-14 09:30:07 +0800

    version of files
    Author: Lionel Liu <bhlx3lyx7@163.com>
    
    Closes #73 from bhlx3lyx7/master.

--- service/src/main/java/org/apache/griffin/core/schedule/SchedulerService.java
+++ service/src/main/java/org/apache/griffin/core/schedule/SchedulerService.java
@@ -1,3 +1,18 @@
+/*-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ */
+
 package org.apache.griffin.core.schedule;
 
 import org.quartz.SchedulerException;
@@ -6,9 +21,6 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by xiangrchen on 6/7/17.
- */
 public interface SchedulerService {
     public List<Map<String, Serializable>> getJobs() throws SchedulerException;
 
