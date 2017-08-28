a3905c7121bf852a5f578e02ad04dd8382470d15
Hang-Hu <huhang600@gmail.com>
2017-08-28 18:00:35 +0800

    adds license

--- service/src/main/java/org/apache/griffin/core/error/exception/RuntimeExceptionHandler.java
+++ service/src/main/java/org/apache/griffin/core/error/exception/RuntimeExceptionHandler.java
@@ -1,5 +1,3 @@
-package org.apache.griffin.core.error.exception;
-
 /*
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
@@ -18,6 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
+package org.apache.griffin.core.error.exception;
 
 import org.apache.griffin.core.util.GriffinOperationMessage;
 import org.slf4j.Logger;
