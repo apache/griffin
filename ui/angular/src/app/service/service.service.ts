41e0007996ca72684bf7826ee645b08b48615fc0
dodobel <1254288433@qq.com>
2018-01-31 10:54:58 +0800

    format code and add scroll bar in measuer view

--- ui/angular/src/app/service/service.service.ts
+++ ui/angular/src/app/service/service.service.ts
@@ -16,103 +16,97 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class ServiceService {
+  constructor() {}
+  //public BACKEND_SERVER = 'http://10.65.145.88:38080';
+  // public BACKEND_SERVER = 'http://localhost:8080';
+  public BACKEND_SERVER = "";
+  public API_ROOT_PATH = "/api/v1";
 
-  constructor() { }
-    //public BACKEND_SERVER = 'http://10.65.145.88:38080';
-    // public BACKEND_SERVER = 'http://localhost:8080';
-    public BACKEND_SERVER = '';
-    public API_ROOT_PATH = '/api/v1';
-    // public ES_SERVER = "http://" + location.host.replace("8080", "9200");
-    // public ES_SERVER = "http://10.64.222.80:39200" ;
-    // public ES_SERVER = "http://10.65.145.88:39200" ;
-    // public ES_SERVER = "http://localhost:8080" ;
+  public config = {
+    // URI paths, always have a trailing /
+    uri: {
+      base: this.BACKEND_SERVER + this.API_ROOT_PATH,
 
-    public config = {
-          // URI paths, always have a trailing /
-          uri: {
-              base: this.BACKEND_SERVER + this.API_ROOT_PATH,
+      login: this.BACKEND_SERVER + this.API_ROOT_PATH + "/login/authenticate",
+      dbtree:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/metadata/hive/dbs/tables",
+      dataassetlist:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/metadata/hive/dbs/tables",
 
-              login:this.BACKEND_SERVER + this.API_ROOT_PATH+ '/login/authenticate',
-              dbtree:this.BACKEND_SERVER + this.API_ROOT_PATH+ '/metadata/hive/dbs/tables',
-              //schemadefinition: this.BACKEND_SERVER + this.API_ROOT_PATH+ '/metadata/hive/table',
-              dataassetlist: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metadata/hive/dbs/tables',
+      getdataasset: this.BACKEND_SERVER + this.API_ROOT_PATH + "/dataassets",
 
-              // adddataasset: this.BACKEND_SERVER + this.API_ROOT_PATH + '/dataassets',
-              // updatedataasset: this.BACKEND_SERVER + this.API_ROOT_PATH + '/dataassets',
-              getdataasset: this.BACKEND_SERVER + this.API_ROOT_PATH + '/dataassets',
-              // deletedataasset: this.BACKEND_SERVER + this.API_ROOT_PATH + '/dataassets',
+      //mydashboard
+      getmydashboard:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics/mydashboard/",
+      // getsubscribe: this.BACKEND_SERVER + this.API_ROOT_PATH + '/subscribe/',
+      // newsubscribe: this.BACKEND_SERVER + this.API_ROOT_PATH + '/subscribe',
 
-              //mydashboard
-              getmydashboard: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/mydashboard/',
-              // getsubscribe: this.BACKEND_SERVER + this.API_ROOT_PATH + '/subscribe/',
-              // newsubscribe: this.BACKEND_SERVER + this.API_ROOT_PATH + '/subscribe',
+      //metrics
 
-              //metrics
+      statistics: this.BACKEND_SERVER + this.API_ROOT_PATH + "/jobs/health",
 
-              statistics: this.BACKEND_SERVER + this.API_ROOT_PATH + '/jobs/health',
+      // briefmetrics: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/briefmetrics',
+      heatmap: this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics/heatmap",
+      // metricdetail: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/complete',
+      // rulemetric: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/brief',
+      //              organization:this.BACKEND_SERVER+'/org',
 
+      orgmap: this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics/org",
 
-              // briefmetrics: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/briefmetrics',
-              heatmap: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/heatmap' ,
-              // metricdetail: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/complete',
-              // rulemetric: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/brief',
-//              organization:this.BACKEND_SERVER+'/org',
+      metricsByOrg: "",
+      //              organization:'/org.json',
+      //              dashboard:'/dashboard.json',
 
-              orgmap: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/org',
+      // organization:this.BACKEND_SERVER + this.API_ROOT_PATH + '/org/measure/jobs',
+      dashboard: this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics",
+      metricdetail:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics/values",
 
+      // dashboard:this.ES_SERVER+'/griffin/accuracy/_search?pretty&filter_path=hits.hits._source',
+      metricsample:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics/sample",
+      metricdownload:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/metrics/download",
 
+      //Models
 
+      allModels: this.BACKEND_SERVER + this.API_ROOT_PATH + "/measures",
+      addModels: this.BACKEND_SERVER + this.API_ROOT_PATH + "/measures",
+      deleteModel: this.BACKEND_SERVER + this.API_ROOT_PATH + "/measures",
+      getModel: this.BACKEND_SERVER + this.API_ROOT_PATH + "/measures",
+      enableModel:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/models/enableModel",
 
-              metricsByOrg:'',
-//              organization:'/org.json',
-//              dashboard:'/dashboard.json',
+      //Jobs
+      allJobs: this.BACKEND_SERVER + this.API_ROOT_PATH + "/jobs",
+      addJobs: this.BACKEND_SERVER + this.API_ROOT_PATH + "/jobs",
+      getMeasuresByOwner:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/measures/owner/",
+      deleteJob: this.BACKEND_SERVER + this.API_ROOT_PATH + "/jobs",
+      getInstances:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/jobs/instances",
 
+      //              allJobs:'/jobs.json',
+      newAccuracyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + "/models",
+      newValidityModel: this.BACKEND_SERVER + this.API_ROOT_PATH + "/models",
+      newAnomalyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + "/models",
+      newPublishModel: this.BACKEND_SERVER + this.API_ROOT_PATH + "/models",
+      // newAccuracyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models/newAccuracyModel' ,
+      // newValidityModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/newValidityModel' ,
+      // newAnomalyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/newAnomalyModel' ,
+      // newPublishModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/newPublishModel' ,
+      // getAccuracyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getAccuracyModel',
+      // getValidityModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getValidityModel',
+      // getPublishModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getPublishModel',
+      // getAnomalyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getAnomalyModel',
 
-              // organization:this.BACKEND_SERVER + this.API_ROOT_PATH + '/org/measure/jobs',
-              dashboard:this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics',
-              metricdetail:this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/values',
-
-
-              // dashboard:this.ES_SERVER+'/griffin/accuracy/_search?pretty&filter_path=hits.hits._source',
-              metricsample: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/sample',
-              metricdownload: this.BACKEND_SERVER + this.API_ROOT_PATH + '/metrics/download',
-
-              //Models
-
-              allModels: this.BACKEND_SERVER + this.API_ROOT_PATH + '/measures',
-              addModels: this.BACKEND_SERVER + this.API_ROOT_PATH + '/measures',
-              deleteModel:this.BACKEND_SERVER + this.API_ROOT_PATH + '/measures',
-              getModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/measures',
-              enableModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models/enableModel',
-
-              //Jobs
-              allJobs:this.BACKEND_SERVER + this.API_ROOT_PATH + '/jobs',
-              addJobs:this.BACKEND_SERVER + this.API_ROOT_PATH + '/jobs',
-              getMeasuresByOwner:this.BACKEND_SERVER + this.API_ROOT_PATH +'/measures/owner/',
-              deleteJob:this.BACKEND_SERVER + this.API_ROOT_PATH + '/jobs',
-              getInstances:this.BACKEND_SERVER + this.API_ROOT_PATH + '/jobs/instances',
-
-//              allJobs:'/jobs.json',
-              newAccuracyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models' ,
-              newValidityModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models' ,
-              newAnomalyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models' ,
-              newPublishModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models' ,
-              // newAccuracyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/models/newAccuracyModel' ,
-              // newValidityModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/newValidityModel' ,
-              // newAnomalyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/newAnomalyModel' ,
-              // newPublishModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/newPublishModel' ,
-              // getAccuracyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getAccuracyModel',
-              // getValidityModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getValidityModel',
-              // getPublishModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getPublishModel',
-              // getAnomalyModel: this.BACKEND_SERVER + this.API_ROOT_PATH + '/model/getAnomalyModel',
-
-              //Notification
-              getnotifications: this.BACKEND_SERVER + this.API_ROOT_PATH + '/notifications'
-          }
-
-      };
+      //Notification
+      getnotifications:
+        this.BACKEND_SERVER + this.API_ROOT_PATH + "/notifications"
+    }
+  };
 }
