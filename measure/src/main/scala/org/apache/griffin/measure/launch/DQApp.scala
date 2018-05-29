13f3d4308b20c509bdabbd7f03409e84220467fd
Grant Guo <grant.xuexu@gmail.com>
2018-05-28 22:57:41 -0400

    refactored the 'measure' portion to have the clearer structure

---
+++ measure/src/main/scala/org/apache/griffin/measure/launch/DQApp.scala
@@ -0,0 +1,52 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+package org.apache.griffin.measure.launch
+
+import org.apache.griffin.measure.Loggable
+import org.apache.griffin.measure.configuration.params.{DQParam, EnvParam}
+
+import scala.util.Try
+
+/**
+  * dq application process
+  */
+trait DQApp extends Loggable with Serializable {
+
+  val envParam: EnvParam
+  val dqParam: DQParam
+
+  def init: Try[_]
+
+  def run: Try[_]
+
+  def close: Try[_]
+
+  /**
+    * application will exit if it fails in run phase.
+    * if retryable is true, the exception will be threw to spark env,
+    * and enable retry strategy of spark application
+    */
+  def retryable: Boolean
+
+  protected def getAppTime: Long = {
+    if (dqParam.timestamp != null && dqParam.timestamp > 0) { dqParam.timestamp }
+    else { System.currentTimeMillis }
+  }
+
+}
