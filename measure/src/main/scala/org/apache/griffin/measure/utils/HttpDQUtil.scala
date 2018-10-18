c7da7d9e78742ad341c63c63dff153a137d965d4
Tanvir Pathan <tanvir.pathan@enquero.com>
2018-10-18 16:37:27 -0500

    Apache license added

--- measure/src/main/scala/org/apache/griffin/measure/utils/HttpDQUtil.scala
+++ measure/src/main/scala/org/apache/griffin/measure/utils/HttpDQUtil.scala
@@ -1,3 +1,21 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
 package org.apache.griffin.measure.utils
 
 import org.apache.http.client.methods.HttpPost
@@ -8,8 +26,9 @@ import org.apache.griffin.measure.Loggable
 
 /**
   * Utility to post Http Requests
-  * While using @HttpUtil.scala, all Http request are getting converted into FsURLConnection scheme, which is causing issue with Spark 2.2.1 onwards
-  * Refer to https://issues.apache.org/jira/browse/SPARK-25694?page=com.atlassian.jira.plugin.system.issuetabpanels%3Aall-tabpanel
+  * While using @HttpUtil.scala, all Http request are getting converted into FsURLConnection scheme,
+  * which is causing issue with Spark 2.2.1 onwards
+  * Refer to <a href='https://issues.apache.org/jira/browse/SPARK-25694'>Here</a>
   */
 object HttpDQUtil extends Loggable {
 
@@ -35,7 +54,7 @@ object HttpDQUtil extends Loggable {
           post.setEntity(new StringEntity(data))
           val resp = client.execute(post)
           debug(s"Status Code: ${resp.getStatusLine.getStatusCode}")
-          (resp.getStatusLine.getStatusCode == 201 || resp.getStatusLine.getStatusCode == 200)
+          resp.getStatusLine.getStatusCode == 201 || resp.getStatusLine.getStatusCode == 200
         case _ =>
           false
       }
