d5ccf5fe7850f76c0f85de02d78e1b62ba5de1e9
Nikolay Sokolov <chemikadze@gmail.com>
2018-10-21 23:15:31 -0700

    Code review fixes

--- service/src/main/java/org/apache/griffin/core/login/ldap/SelfSignedSocketFactory.java
+++ service/src/main/java/org/apache/griffin/core/login/ldap/SelfSignedSocketFactory.java
@@ -1,3 +1,22 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
 package org.apache.griffin.core.login.ldap;
 
 import javax.net.SocketFactory;
@@ -12,15 +31,25 @@ import java.net.UnknownHostException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+/**
+ * SocketFactory ignoring insecure (self-signed, expired) certificates.
+ *
+ * Maintains internal {@code SSLSocketFactory} configured with {@code NoopTrustManager}.
+ * All SocketFactory methods are proxied to internal SSLSocketFactory instance.
+ * Accepts all client and server certificates, from any issuers.
+ */
 public class SelfSignedSocketFactory extends SocketFactory {
     private SSLSocketFactory sf;
 
-    public SelfSignedSocketFactory() throws Exception {
+    private SelfSignedSocketFactory() throws Exception {
         SSLContext ctx = SSLContext.getInstance("TLS");
         ctx.init(null, new TrustManager[]{new NoopTrustManager()}, null);
         sf = ctx.getSocketFactory();
     }
 
+    /**
+     * Part of SocketFactory contract, used by javax.net internals to create new instance.
+     */
     public static SocketFactory getDefault() {
         try {
             return new SelfSignedSocketFactory();
@@ -29,6 +58,9 @@ public class SelfSignedSocketFactory extends SocketFactory {
         }
     }
 
+    /**
+     * Insecure trust manager accepting any client and server certificates.
+     */
     public static class NoopTrustManager implements X509TrustManager {
         public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
         }
