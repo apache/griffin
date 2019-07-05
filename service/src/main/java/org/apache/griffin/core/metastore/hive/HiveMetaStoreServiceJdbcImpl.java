a60d469a386c62b6e5a585498a4048f096b8d61e
qwang8 <qwang8@ebay.com>
2019-07-04 21:21:27 -0700

    add Apache header, do checkStyle, and add unit tests

--- service/src/main/java/org/apache/griffin/core/metastore/hive/HiveMetaStoreServiceJdbcImpl.java
+++ service/src/main/java/org/apache/griffin/core/metastore/hive/HiveMetaStoreServiceJdbcImpl.java
@@ -1,4 +1,34 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
 package org.apache.griffin.core.metastore.hive;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
@@ -11,20 +41,14 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-/**
- * Author: qwang8
- * Date:   2019-06-18.
- */
+
+
+
 @Service
 @Qualifier(value = "hive_jdbc")
-@CacheConfig(cacheNames = "hive", keyGenerator = "cacheKeyGenerator")
-public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
+@CacheConfig(cacheNames = "jdbchive", keyGenerator = "cacheKeyGenerator")
+public class HiveMetaStoreServiceJdbcImpl implements HiveMetaStoreService {
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(HiveMetaStoreService.class);
@@ -33,7 +57,17 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
     private String hiveClassName;
 
     @Value("${hive.jdbc.url}")
-    private String hiveURL;
+    private String hiveUrl;
+
+    private Connection conn;
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void setHiveClassName(String hiveClassName) {
+        this.hiveClassName = hiveClassName;
+    }
 
     @Override
     @Cacheable(unless = "#result==null")
@@ -61,20 +95,11 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
         return res;
     }
 
-    /**
-     * Not recommend use this method, because it will get connections for every table
-     * @param db
-     * @return
-     */
     @Override
     public List<Table> getAllTable(String db) {
         return null;
     }
 
-    /**
-     * Not recommend use this method, because it will get connections for every table
-     * @return
-     */
     @Override
     public Map<String, List<Table>> getAllTable() {
         return null;
@@ -88,14 +113,15 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
         result.setTableName(tableName);
 
         String sql = "show create table " + dbName + "." + tableName;
-        Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
         StringBuilder sb = new StringBuilder();
 
         try {
             Class.forName(hiveClassName);
-            conn = DriverManager.getConnection(hiveURL);
+            if (conn == null) {
+                conn = DriverManager.getConnection(hiveUrl);
+            }
             LOGGER.info("got connection");
             stmt = conn.createStatement();
             rs = stmt.executeQuery(sql);
@@ -113,9 +139,15 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
             e.printStackTrace();
         } finally {
             try {
-                if (rs != null) rs.close();
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -130,18 +162,19 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
 
     /**
      * Query Hive for Show tables or show databases, which will return List of String
-     * @param sql
+     * @param sql sql string
      * @return
      */
     private Iterable<String> queryHiveString(String sql) {
         List<String> res = new ArrayList<>();
-        Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
 
         try {
             Class.forName(hiveClassName);
-            conn = DriverManager.getConnection(hiveURL);
+            if (conn == null) {
+                conn = DriverManager.getConnection(hiveUrl);
+            }
             LOGGER.info("got connection");
             stmt = conn.createStatement();
             rs = stmt.executeQuery(sql);
@@ -152,9 +185,15 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
             e.printStackTrace();
         } finally {
             try {
-                if (rs != null) rs.close();
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -170,12 +209,16 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
     public String getLocation(String tableMetadata) {
         tableMetadata = tableMetadata.toLowerCase();
         int index = tableMetadata.indexOf("location");
-        if (index == -1) return "";
+        if (index == -1) {
+            return "";
+        }
 
         int start = tableMetadata.indexOf("\'", index);
         int end = tableMetadata.indexOf("\'", start + 1);
 
-        if (start == -1 || end == -1) return "";
+        if (start == -1 || end == -1) {
+            return "";
+        }
 
         return tableMetadata.substring(start + 1, end);
     }
@@ -205,28 +248,36 @@ public class HiveMetaStoreServiceJDBCImpl implements HiveMetaStoreService {
     /**
      * Parse one column string, such as : `merch_date` string COMMENT 'this is merch process date'
      *
-     * @param colStr
+     * @param colStr column string
      * @return
      */
     public String getComment(String colStr) {
         colStr = colStr.toLowerCase();
         int i = colStr.indexOf("comment");
-        if (i == -1) return "";
+        if (i == -1) {
+            return "";
+        }
 
-        int s = -1, e = -1;
+        int s = -1;
+        int e = -1;
         while (i < colStr.length()) {
             if (colStr.charAt(i) == '\'') {
-                if (s == -1) s = i;
-                else {
+                if (s == -1) {
+                    s = i;
+                } else {
                     e = i;
                     break;
                 }
             }
             i++;
         }
-        if (s == -1 || e == -1) return "";
-        if (s > e) return "";
+        if (s == -1 || e == -1) {
+            return "";
+        }
+        if (s > e) {
+            return "";
+        }
 
-        return colStr.substring(s+1, e);
+        return colStr.substring(s + 1, e);
     }
 }
---
+++ service/src/test/java/org/apache/griffin/core/metastore/hive/HiveMetastoreServiceJDBCImplTest.java
@@ -0,0 +1,121 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package org.apache.griffin.core.metastore.hive;
+
+
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.*;
+import java.util.List;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+@RunWith(SpringRunner.class)
+public class HiveMetastoreServiceJDBCImplTest {
+
+    private HiveMetaStoreServiceJdbcImpl serviceJDBC = new HiveMetaStoreServiceJdbcImpl();
+
+    @Mock
+    private Connection conn;
+
+    @Mock
+    private Statement stmt;
+
+    @Mock
+    private ResultSet rs;
+
+    @Before
+    public void setUp() throws SQLException {
+        serviceJDBC.setConn(conn);
+        serviceJDBC.setHiveClassName("org.apache.hive.jdbc.HiveDriver");
+    }
+
+    @Test
+    public void testGetComment() {
+        String colStr = "`merch_date` string COMMENT 'this is merch date'";
+        String comment = serviceJDBC.getComment(colStr);
+        assert (comment.equals("this is merch date"));
+
+        colStr = "`merch_date` string COMMENT ''";
+        comment = serviceJDBC.getComment(colStr);
+        Assert.assertTrue(comment.isEmpty());
+    }
+
+    @Test
+    public void testgetAllDatabases() throws SQLException {
+        when(conn.createStatement()).thenReturn(stmt);
+        when(stmt.executeQuery(anyString())).thenReturn(rs);
+        when(rs.next()).thenReturn(true).thenReturn(false);
+        when(rs.getString(anyInt())).thenReturn("default");
+
+        Iterable<String> res = serviceJDBC.getAllDatabases();
+        for (String s : res) {
+            Assert.assertEquals(s, "default");
+            break;
+        }
+    }
+
+    @Test
+    public void testGetAllTableNames() throws SQLException {
+        when(conn.createStatement()).thenReturn(stmt);
+        when(stmt.executeQuery(anyString())).thenReturn(rs);
+        when(rs.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(rs.getString(anyInt())).thenReturn("merch_data").thenReturn("merch_summary");
+
+        Iterable<String> res = serviceJDBC.getAllTableNames("default");
+        StringBuilder sb = new StringBuilder();
+        for (String s : res) {
+            sb.append(s).append(",");
+        }
+        Assert.assertEquals(sb.toString(), "merch_data,merch_summary,");
+    }
+
+    @Test
+    public void testGetTable() throws SQLException {
+        String meta = "CREATE EXTERNAL TABLE `default.merch_data`(  `merch_date` string COMMENT 'this is merch date',   `site_id` int COMMENT '',   `guid` string COMMENT '',   `user_id` string COMMENT '',   `treatments` string COMMENT '',   `experiments` string COMMENT '',   `page_id` int COMMENT '',   `placement_id` int COMMENT '',   `meid` string COMMENT '',   `program_id` bigint COMMENT '',   `module_id` int COMMENT '',   `algorithm_selected_config_id` bigint COMMENT '',   `algorithm_id` int COMMENT '',   `mbe` string COMMENT '',   `issps` string COMMENT '',   `mbe_version` string COMMENT '',   `trks` string COMMENT '',   `session_skey` string COMMENT '',   `seqnum` string COMMENT '',   `channel` int COMMENT '',   `icf` string COMMENT '',   `device` string COMMENT '',   `device_exp` string COMMENT '',   `experience` string COMMENT '',   `euid` string COMMENT '',   `impression_count` bigint COMMENT '',   `surface_plmt_imp` bigint COMMENT '',   `plmt_imp` bigint COMMENT '',   `oi` int COMMENT '',   `in` string COMMENT '',   `seeditem` string COMMENT '',   `seeditem_v2` array<string> COMMENT '',   `multi_seeds` array<string> COMMENT '',   `out` array<string> COMMENT '',   `vi` array<string> COMMENT '',   `bid` array<string> COMMENT '',   `bin` array<string> COMMENT '',   `offer` array<string> COMMENT '',   `watch` array<string> COMMENT '',   `asq` array<string> COMMENT '',   `add2cart` array<string> COMMENT '',   `add2list` array<string> COMMENT '',   `purchase_items` array<string> COMMENT '',   `purchase` bigint COMMENT '',   `migmb` bigint COMMENT '',   `mbe_value` string COMMENT '',   `out_deals` string COMMENT '',   `collection` array<string> COMMENT '',   `lndpg_click` array<string> COMMENT '')COMMENT 'merch_data for merch team'PARTITIONED BY (   `dt` string,   `placement` int)ROW FORMAT SERDE   'org.apache.hadoop.hive.serde2.avro.AvroSerDe' STORED AS INPUTFORMAT   'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat' OUTPUTFORMAT   'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'LOCATION  'hdfs://ares-lvs-nn-ha/apps/hdmi-set/buyexp/merch/common/merch_data'TBLPROPERTIES (  'COLUMN_STATS_ACCURATE'='false',   'avro.schema.url'='hdfs://ares-lvs-nn-ha/user/b_bis/merch/avro/merch-data-1.0.avsc',   'transient_lastDdlTime'='1535651637')";
+        when(conn.createStatement()).thenReturn(stmt);
+        when(stmt.executeQuery(anyString())).thenReturn(rs);
+        when(rs.next()).thenReturn(true).thenReturn(false);
+        when(rs.getString(anyInt())).thenReturn(meta);
+
+        Table res = serviceJDBC.getTable("default", "merch_data");
+
+        assert (res.getDbName().equals("default"));
+        assert (res.getTableName().equals("merch_data"));
+        assert (res.getSd().getLocation().equals("hdfs://ares-lvs-nn-ha/apps/hdmi-set/buyexp/merch/common/merch_data"));
+        List<FieldSchema> fieldSchemas = res.getSd().getCols();
+        for (FieldSchema fieldSchema : fieldSchemas) {
+            Assert.assertEquals(fieldSchema.getName(),"merch_date");
+            Assert.assertEquals(fieldSchema.getType(),"string");
+            Assert.assertEquals(fieldSchema.getComment(),"this is merch date");
+            break;
+        }
+    }
+}
