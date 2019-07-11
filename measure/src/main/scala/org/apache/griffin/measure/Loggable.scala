eb543789707b83ae0e1aa02ce98bcd395862ebb6
Johnnie <joohnnie.z@gmail.com>
2019-07-11 14:37:04 -0700

    GRIFFIN-267 use call by name for logger

--- measure/src/main/scala/org/apache/griffin/measure/Loggable.scala
+++ measure/src/main/scala/org/apache/griffin/measure/Loggable.scala
@@ -24,27 +24,27 @@ trait Loggable {
 
   @transient private lazy val logger = LoggerFactory.getLogger(getClass)
 
-  protected def info(msg: String): Unit = {
+  protected def info(msg: => String): Unit = {
     logger.info(msg)
   }
 
-  protected def debug(msg: String): Unit = {
+  protected def debug(msg: => String): Unit = {
     logger.debug(msg)
   }
 
-  protected def warn(msg: String): Unit = {
+  protected def warn(msg: => String): Unit = {
     logger.warn(msg)
   }
 
-  protected def warn(msg: String, e: Throwable): Unit = {
+  protected def warn(msg: => String, e: Throwable): Unit = {
     logger.warn(msg, e)
   }
 
-  protected def error(msg: String): Unit = {
+  protected def error(msg: => String): Unit = {
     logger.error(msg)
   }
 
-  protected def error(msg: String, e: Throwable): Unit = {
+  protected def error(msg: => String, e: Throwable): Unit = {
     logger.error(msg, e)
   }
 
