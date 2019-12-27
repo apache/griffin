9d27553d4353288210a1297afd641acf8db3fe43
tusharpatil20 <tushargp20@gmail.com>
2019-12-27 11:31:04 +0530

    [Griffin-315] Adding JDBC based data connector

---
+++ measure/src/main/scala/org/apache/griffin/measure/datasource/connector/batch/JDBCBasedDataConnector.scala
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.griffin.measure.datasource.connector.batch
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import org.apache.griffin.measure.Loggable
+import org.apache.griffin.measure.configuration.dqdefinition.DataConnectorParam
+import org.apache.griffin.measure.context.TimeRange
+import org.apache.griffin.measure.datasource.TimestampStorage
+import org.apache.griffin.measure.utils.ParamUtil._
+
+/**
+ * A batch data connector for file based sources which allows support various
+ * file based data sources like Parquet, CSV, TSV, ORC etc.
+ * Local files can also be read by prepending `file://` namespace.
+ *
+ * Currently supported formats like Parquet, ORC, AVRO, Text and Delimited types like CSV, TSV etc.
+ *
+ * Supported Configurations:
+ *  - database : [[String]] specifying the database name.
+ *  - tablename : [[String]] specifying the table name to be read
+ *  - url : [[String]] specifying the URL to connect to database
+ *  - user : [[String]] specifying the user for connection to database
+ *  - password: [[String]] specifying the password for connection to database
+ *  - driver : [[String]] specifying the driver for JDBC connection to database
+ *  - where : [[String]] specifying the condition for reading data from table
+ *
+ * Some defaults assumed by this connector (if not set) are as follows:
+ *  - `database` is default,
+ *  - `driver` is com.mysql.jdbc.Driver,
+ *  - `where` is None
+ */
+case class JDBCBasedDataConnector(
+    @transient sparkSession: SparkSession,
+    dcParam: DataConnectorParam,
+    timestampStorage: TimestampStorage)
+    extends BatchDataConnector {
+
+  import JDBCBasedDataConnector._
+  val config: Map[String, Any] = dcParam.getConfig
+  val database: String = config.getString(Database, DefaultDatabase)
+  val tableName: String = config.getString(TableName, EmptyString)
+  val fullTableName: String = s"$database.$tableName"
+  val whereString: String = config.getString(Where, EmptyString)
+  val url: String = config.getString(Url, EmptyString)
+  val user: String = config.getString(User, EmptyString)
+  val password: String = config.getString(Password, EmptyString)
+  val driver: String = config.getString(Driver, DefaultDriver)
+
+  require(url.nonEmpty, "JDBC connection: connection url is mandatory")
+  require(user.nonEmpty, "JDBC connection: user name is mandatory")
+  require(password.nonEmpty, "JDBC connection: password is mandatory")
+  require(tableName.nonEmpty, "JDBC connection: table is mandatory")
+  assert(isJDBCDriverLoaded(driver), s"JDBC driver ${driver} not present in classpath")
+
+  override def data(ms: Long): (Option[DataFrame], TimeRange) = {
+    val dfOpt = try {
+      val dtSql = createSqlStmt()
+      val prop = new java.util.Properties
+      prop.setProperty("user", user)
+      prop.setProperty("password", password)
+      prop.setProperty("driver", driver)
+      val df: DataFrame = sparkSession.read.jdbc(url, s"($dtSql) as t", prop)
+      val dfOpt = Some(df)
+      val preDfOpt = preProcess(dfOpt, ms)
+      preDfOpt
+    } catch {
+      case e: Throwable =>
+        error(s"load mysql table $fullTableName fails: ${e.getMessage}", e)
+        None
+    }
+    val tmsts = readTmst(ms)
+    (dfOpt, TimeRange(ms, tmsts))
+  }
+  private def createSqlStmt(): String = {
+
+    val wheres = whereString.split(",").map(_.trim).filter(_.nonEmpty)
+    val tableClause = s"SELECT * FROM $fullTableName"
+    if (wheres.length > 0) {
+      val clauses = wheres.map { w =>
+        s"$tableClause WHERE $w"
+      }
+      clauses.mkString(" UNION ALL ")
+    } else tableClause
+  }
+}
+object JDBCBasedDataConnector extends Loggable {
+  private val Database: String = "database"
+  private val TableName: String = "tablename"
+  private val Where: String = "where"
+  private val Url: String = "url"
+  private val User: String = "user"
+  private val Password: String = "password"
+  private val Driver: String = "driver"
+
+  private val DefaultDriver = "com.mysql.jdbc.Driver"
+  private val DefaultDatabase = "default"
+  private val EmptyString = ""
+
+  private def isJDBCDriverLoaded(driver: String): Boolean = {
+    try {
+      Class.forName(driver, false, this.getClass.getClassLoader)
+      true
+    } catch {
+      case x: ClassNotFoundException =>
+        griffinLogger.error("JDBC driver provided is not found in class path")
+        false
+      case _ =>
+        griffinLogger.info("JDBC driver provided present in class path")
+        true
+    }
+  }
+}
