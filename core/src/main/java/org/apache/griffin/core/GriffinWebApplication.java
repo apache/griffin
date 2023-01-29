3766fbb3cb7904896fc0559333123ed9d820cdf7
Warden <wangding2587@gmail.com>
2023-01-29 23:41:58 +0800

    the proto file of tm and en

--- core/src/main/java/org/apache/griffin/core/GriffinWebApplication.java
+++ core/src/main/java/org/apache/griffin/core/GriffinWebApplication.java
@@ -18,12 +18,10 @@ under the License.
 */
 package org.apache.griffin.core;
 
-import org.apache.griffin.api.proto.protocol.CommonReply;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
