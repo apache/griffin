193c12294c1aafd5b808dd071e168de4e9f5909e
William Guo <guoyp@apache.org>
2024-08-13 11:58:56 +0800

    add metric definition

--- griffin-metric/src/main/java/org/apache/griffin/metric/model/BaseEntity.java
+++ griffin-metric/src/main/java/org/apache/griffin/metric/model/BaseEntity.java
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
+/*
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
@@ -8,7 +7,7 @@ to you under the Apache License, Version 2.0 (the
 "License"); you may not use this file except in compliance
 with the License.  You may obtain a copy of the License at
 
-  https://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
 software distributed under the License is distributed on an
@@ -16,21 +15,41 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
--->
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+*/
+package org.apache.griffin.metric.model;
 
-    <modelVersion>4.0.0</modelVersion>
+import java.util.Date;
 
-    <parent>
-        <groupId>org.apache.griffin</groupId>
-        <artifactId>griffin</artifactId>
-        <version>2.0.0-SNAPSHOT</version>
-    </parent>
-
-    <artifactId>griffin-metric</artifactId>
-    <name>${project.artifactId}</name>
-    <packaging>jar</packaging>
+import lombok.Data;
 
 
-</project>
+
+@Data
+public abstract class BaseEntity implements java.io.Serializable {
+
+    private static final long serialVersionUID = 2110740953277261851L;
+
+    /**
+     * creation time
+     */
+    protected Date ctime;
+
+    /**
+     * update time
+     */
+    protected Date mtime;
+
+    public void prePersist() {
+        Date date = new Date();
+        if (ctime == null) {
+            ctime = date;
+        }
+        if (mtime == null) {
+            mtime = date;
+        }
+    }
+
+    public void preUpdate() {
+        mtime = new Date();
+    }
+}
--- griffin-metric/src/main/java/org/apache/griffin/metric/model/MetricD.java
+++ griffin-metric/src/main/java/org/apache/griffin/metric/model/MetricD.java
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
+/*
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
@@ -8,7 +7,7 @@ to you under the Apache License, Version 2.0 (the
 "License"); you may not use this file except in compliance
 with the License.  You may obtain a copy of the License at
 
-  https://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
 software distributed under the License is distributed on an
@@ -16,21 +15,27 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
--->
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+*/
+package org.apache.griffin.metric.model;
 
-    <modelVersion>4.0.0</modelVersion>
+import lombok.*;
 
-    <parent>
-        <groupId>org.apache.griffin</groupId>
-        <artifactId>griffin</artifactId>
-        <version>2.0.0-SNAPSHOT</version>
-    </parent>
+import java.util.Map;
 
-    <artifactId>griffin-metric</artifactId>
-    <name>${project.artifactId}</name>
-    <packaging>jar</packaging>
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class MetricD extends BaseEntity {
 
+    private long metricId;
 
-</project>
+    private String metricName;
+
+    private String owner;
+
+    private String description;
+
+    private Map<String, String> tags;
+}
